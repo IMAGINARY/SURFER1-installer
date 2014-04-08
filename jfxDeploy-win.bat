70311c0ea87f1b65a09a8a12c9e0df9ed7e71402
porst17 <stussak@mfo.de>
2014-04-08 16:56:54 +0200

    small script for simpler building on Windows

---
+++ jfxDeploy-win.bat
@@ -0,0 +1,6 @@
+REM Adjust paths to your own needs!!
+REM ================================
+REM
+SET JAVA_HOME=C:\Program Files\Java\jdk1.7.0_45
+SET PATH=C:\Program Files\Inno Setup 5
+gradlew clean jfxDeploy
