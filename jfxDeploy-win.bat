e83b33f2183fff37bdfb0f18625e4bccb3737109
porst17 <stussak@mfo.de>
2014-05-07 14:31:15 +0200

    SURFER 1.0.3

--- jfxDeploy-win.bat
+++ jfxDeploy-win.bat
@@ -2,5 +2,5 @@ REM Adjust paths to your own needs!!
 REM ================================
 REM
 SET JAVA_HOME=C:\Program Files\Java\jdk1.7.0_45
-SET PATH=C:\Program Files\Inno Setup 5
+SET PATH=C:\Program Files\Inno Setup 5;C:\Program Files\WiX;%PATH%
 gradlew clean jfxDeploy
