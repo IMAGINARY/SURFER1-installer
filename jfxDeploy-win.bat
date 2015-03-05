61cd7b5d8c83cd7303bdb2451b202195c05df358
porst17 <stussak@mfo.de>
2015-03-05 16:38:52 +0100

    change to script directory before deploying

--- jfxDeploy-win.bat
+++ jfxDeploy-win.bat
@@ -1,6 +1,7 @@
 REM Adjust paths to your own needs!!
 REM ================================
 REM
-SET JAVA_HOME=C:\Program Files\Java\jdk1.7.0_45
+SET JAVA_HOME=C:\Program Files\Java\jdk1.7.0_76
 SET PATH=C:\Program Files\Inno Setup 5;C:\Program Files\WiX;%PATH%
+cd /d "%~dp0"
 gradlew clean jfxDeploy
