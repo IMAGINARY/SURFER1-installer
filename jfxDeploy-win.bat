d3b549478f2efc287e95ee92c2e6c725663965a0
Christian Stussak <stussak@mfo.de>
2017-07-09 10:29:09 +0200

    SURFER 1.5.0: Fix gradle dependency resolution; fix Windows build script; Fix locale selection for locales containing language and country code; Fix missing '0=' when printing surfaces with short formula; Add Taiwanese translation (zh_TW)

--- jfxDeploy-win.bat
+++ jfxDeploy-win.bat
@@ -1,7 +1,8 @@
 REM Adjust paths to your own needs!!
 REM ================================
 REM
-SET JAVA_HOME=C:\Program Files\Java\jdk1.7.0_76
+SET JAVA_HOME=C:\Program Files\Java\jdk1.7.0_80
 SET PATH=C:\Program Files\Inno Setup 5;C:\Program Files (x86)\Inno Setup 5;C:\Program Files\WiX;%PATH%
 cd /d "%~dp0"
-gradlew clean jfxDeploy
+REM gradlew clean jfxDeploy
+gradlew jfxDeploy
